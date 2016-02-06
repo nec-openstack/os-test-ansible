ae0dfe87553e0c7bba2dbaed19bfa37579ec7e74
hidekazuna <hidekazuna@gmail.com>
2016-02-06 22:31:52 +0900

    Update create public network script
    Updated create_public_network.sh to use floating IP.

--- scripts/create_public_network.sh
+++ scripts/create_public_network.sh
@@ -6,9 +6,9 @@ END_IP_ADDRESS=203.0.113.200
 DNS_RESOLVER=8.8.4.4
 PUBLIC_NETWORK_GATEWAY=203.0.113.1
 
-neutron net-create public --router:external --shared --provider:physical_network public --provider:network_type flat
+neutron net-create public --router:external --provider:physical_network public --provider:network_type flat
 
 neutron subnet-create public $PUBLIC_NETWORK_CIDR --name public \
   --allocation-pool start=$START_IP_ADDRESS,end=$END_IP_ADDRESS \
-  --dns-nameserver $DNS_RESOLVER --gateway $PUBLIC_NETWORK_GATEWAY
+  --dns-nameserver $DNS_RESOLVER --disable-dhcp --gateway $PUBLIC_NETWORK_GATEWAY
 
