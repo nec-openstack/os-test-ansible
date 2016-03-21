cb0d2d6bf029734eaf2da9c27aae7f936800a3de
hidekazuna <hidekazuna@gmail.com>
2016-03-21 23:12:39 +0900

    Add block storage service

---
+++ playbooks/roles/cinder/files/create_cinder_service_entity_and_api_endpoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+openstack user create --domain default --password $1 cinder
+openstack role add --project service --user cinder admin
+openstack service create --name cinder --description "OpenStack Block Storage" volume
+openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
+openstack endpoint create --region RegionOne volume public http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume internal http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume admin http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2/%\(tenant_id\)s
