038fd80e6a08c4e64954d7c7806b81b91b3360bb
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-06-01 15:18:05 +0900

    Use utils to create nova endpoint/service

--- playbooks/roles/nova/files/create_nova_service_entity_and_api_endpoint.sh
+++ playbooks/roles/nova/files/create_nova_service_entity_and_api_endpoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-openstack user create --domain default --password $1 nova
-openstack role add --project service --user nova admin
-openstack service create --name nova --description "OpenStack Compute" compute
-openstack endpoint create --region RegionOne compute public http://$2:8774/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne compute internal http://$2:8774/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne compute admin http://$2:8774/v2/%\(tenant_id\)s
+source /opt/local/bin/os-utils.sh
+
+create_or_get_user nova $1
+get_or_add_user_project_role admin nova service
+create_or_get_service nova compute "OpenStack Compute"
+create_or_get_endpoint compute public http://$2:8774/v2/%\(tenant_id\)s
+create_or_get_endpoint compute internal http://$2:8774/v2/%\(tenant_id\)s
+create_or_get_endpoint compute admin http://$2:8774/v2/%\(tenant_id\)s
