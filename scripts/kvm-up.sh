9288050fafa50dfabd07fb76b4c4f62a80a5bbba
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-06-02 15:57:54 +0900

    Add ssh_target function

--- scripts/kvm-up.sh
+++ scripts/kvm-up.sh
@@ -38,6 +38,14 @@ function wait_host {
   fi
 }
 
+function ssh_target {
+  local target=$1
+  local script=$2
+  ssh operator@${target} -o StrictHostKeyChecking=no \
+                         -o UserKnownHostsFile=/dev/null \
+                         "${script}"
+}
+
 for setting in "${settings[@]}"
 do
     setting=($setting)
@@ -60,14 +68,14 @@ do
                              --model virtio \
                              ${setting[1]}
       sleep 1
-      ssh operator@${api_network}.${setting[5]} "sudo echo 'auto eth1' > /etc/network/interfaces.d/eth1.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo 'iface eth1 inet static' >> /etc/network/interfaces.d/eth1.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo '  address ${tunnel_network}.${setting[5]}' >> /etc/network/interfaces.d/eth1.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo '  netmask ${netmask}' >> /etc/network/interfaces.d/eth1.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo '  gateway ${gateway}' >> /etc/network/interfaces.d/eth1.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo '  dns-nameservers 8.8.8.8' >> /etc/network/interfaces.d/eth1.cfg"
-      ssh operator@${api_network}.${setting[5]} "ifdown eth1"
-      ssh operator@${api_network}.${setting[5]} "ifup eth1"
+      ssh_target ${api_network}.${setting[5]} "sudo echo 'auto eth1' > /etc/network/interfaces.d/eth1.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo 'iface eth1 inet static' >> /etc/network/interfaces.d/eth1.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo '  address ${tunnel_network}.${setting[5]}' >> /etc/network/interfaces.d/eth1.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo '  netmask ${netmask}' >> /etc/network/interfaces.d/eth1.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo '  gateway ${gateway}' >> /etc/network/interfaces.d/eth1.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo '  dns-nameservers 8.8.8.8' >> /etc/network/interfaces.d/eth1.cfg"
+      ssh_target ${api_network}.${setting[5]} "ifdown eth1"
+      ssh_target ${api_network}.${setting[5]} "ifup eth1"
     fi
 
     if [[ ${setting[0]} = 'network' ]]; then
@@ -77,12 +85,12 @@ do
                              --model virtio \
                              ${setting[1]}
       sleep 1
-      ssh operator@${api_network}.${setting[5]} "sudo echo 'auto eth2' > /etc/network/interfaces.d/eth2.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo 'iface eth2 inet manual' >> /etc/network/interfaces.d/eth2.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo '  up ip link set dev eth2 up' >> /etc/network/interfaces.d/eth2.cfg"
-      ssh operator@${api_network}.${setting[5]} "sudo echo '  down ip link set dev eth2 down' >> /etc/network/interfaces.d/eth2.cfg"
-      ssh operator@${api_network}.${setting[5]} "ifdown eth2"
-      ssh operator@${api_network}.${setting[5]} "ifup eth2"
+      ssh_target ${api_network}.${setting[5]} "sudo echo 'auto eth2' > /etc/network/interfaces.d/eth2.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo 'iface eth2 inet manual' >> /etc/network/interfaces.d/eth2.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo '  up ip link set dev eth2 up' >> /etc/network/interfaces.d/eth2.cfg"
+      ssh_target ${api_network}.${setting[5]} "sudo echo '  down ip link set dev eth2 down' >> /etc/network/interfaces.d/eth2.cfg"
+      ssh_target ${api_network}.${setting[5]} "ifdown eth2"
+      ssh_target ${api_network}.${setting[5]} "ifup eth2"
     fi
 
 done
